ac11a4dc4622f4bbd35e79f65e1372d9a86b577a
Karolina <11.karolinakucia@gmail.com>
2025-11-27 23:05:54 +0100

    fix

---
+++ src/main/java/com/prz/edu/pl/ogloszone/OgloszoneApplication.java
@@ -0,0 +1,13 @@
+package com.prz.edu.pl.ogloszone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OglosZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OglosZoneApplication.class, args);
+	}
+
+}
